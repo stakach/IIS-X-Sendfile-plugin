9b65f3880b15ac3769adb76724155e52009c1753
Stephen von Takach <steve@advancedcontrol.com.au>
2011-09-02 07:58:39 +1000

    Minor update to provide more flexibility

--- XSendfile/XSendfileHttpModule.cs
+++ XSendfile/XSendfileHttpModule.cs
@@ -21,17 +21,27 @@ namespace XSendfile
         {
             HttpResponse response = HttpContext.Current.Response;
 
+            //
+            // Check for the X-Send headers
+            //
             string filePath = response.Headers.Get("X-Sendfile");
             if (filePath == null)
                 filePath = response.Headers.Get("X-Accel-Redirect");
 
             if (filePath != null)
             {
+                //
+                // Determine the file path and ready teh response
+                //
                 filePath = Path.Combine(ConfigurationManager.AppSettings["XSendDir"], filePath);
                 response.Clear();                               // Clears output buffer
                 response.Headers.Remove("X-Sendfile");          // Remove unwanted headers
                 response.Headers.Remove("X-Accel-Redirect");
 
+
+                //
+                // Set the cache policy
+                //
                 if(ConfigurationManager.AppSettings["XSendCache"] == null)
                     response.Cache.SetCacheability(HttpCacheability.NoCache);
                 else if(ConfigurationManager.AppSettings["XSendCache"] == "Public")
@@ -39,23 +49,44 @@ namespace XSendfile
                 else
                     response.Cache.SetCacheability(HttpCacheability.Private);
 
+
+                //
+                // Get the file information and set headers appropriately
+                //
                 FileInfo file = new FileInfo(filePath);
                 response.Cache.SetLastModified(file.LastWriteTimeUtc);
+                response.Headers.Remove("Content-Length");
                 response.AddHeader("Content-Length", file.Length.ToString());
 
 
-                //System.Configuration.Configuration staticContent = ConfigurationManager.GetSection("staticContent");
-                //var mimeMap = staticContent.GetCollection();
-                //var mt = mimeMap.Where(
-                //    a => (string)a.Attributes["fileExtension"].Value == ".pdf"
-                //    ).FirstOrDefault();
+                //
+                // Check if we want to detect the mime type of the current content
+                //
+                if (ConfigurationManager.AppSettings["XSendMime"] == null)
+                {
+                    //System.Configuration.Configuration staticContent = ConfigurationManager.GetSection("staticContent");
+                    //var mimeMap = staticContent.GetCollection();
+                    //var mt = mimeMap.Where(
+                    //    a => (string)a.Attributes["fileExtension"].Value == ".pdf"
+                    //    ).FirstOrDefault();
 
-                //if (mt != null)
-                //    response.ContentType = mt["mimeType"];
-                //else
+                    //if (mt != null)
+                    //    response.ContentType = mt["mimeType"];
+                    //else
                     response.ContentType = "application/octet-stream";
+                }
 
-                response.AppendHeader("Content-Disposition", "inline;filename=" + file.Name);
+
+                //
+                // Set a content disposition if it is not already set by the application
+                //
+                if (response.Headers["Content-Disposition"] == null)
+                    response.AppendHeader("Content-Disposition", "inline;filename=" + file.Name);
+
+
+                //
+                //  Send the file without loading it into memory
+                //
                 response.TransmitFile(file.FullName);
             }
         }
