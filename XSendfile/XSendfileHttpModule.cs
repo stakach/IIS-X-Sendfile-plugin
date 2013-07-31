8c64aaa38083d3e16841ebec14e22690e3fc22d1
Stephen von Takach <steve@webcontrol.me>
2013-07-31 17:02:29 +1000

    add support for range requests
    UNTESTED

--- XSendfile/XSendfileHttpModule.cs
+++ XSendfile/XSendfileHttpModule.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web;
 using System.Web.Hosting;
+using System.Web.Util;
 using System.Configuration;
 using System.IO;
+using System.Runtime.InteropServices;
 
 //
 // This feels hacky
@@ -25,6 +28,7 @@ namespace XSendfile
         private void context_EndRequest(object sender, EventArgs e)
         {
             HttpResponse response = HttpContext.Current.Response;
+            HttpRequest request = HttpContext.Current.Request;
 
             //
             // Check for the X-Send headers
@@ -62,10 +66,41 @@ namespace XSendfile
                 //
                 // Get the file information and set headers appropriately
                 //
+                if (!FileUtil.FileExists(filePath))
+                {
+                    throw new HttpException(404, "File_does_not_exist");
+                }
+                if (filePath[filePath.Length - 1] == '.')
+                {
+                    throw new HttpException(404, "File_does_not_exist");
+                }
                 FileInfo file = new FileInfo(filePath);
                 response.Cache.SetLastModified(file.LastWriteTimeUtc);
                 response.Headers.Remove("Content-Length");
+
+                DateTime dateTime = new DateTime(file.LastWriteTime.Year, file.LastWriteTime.Month, file.LastWriteTime.Day, file.LastWriteTime.Hour, file.LastWriteTime.Minute, file.LastWriteTime.Second, 0);
+                DateTime now = DateTime.Now;
+                string range = request.Headers["Range"];
+
+                //
+                // Call into .net static file handler to do the heavy lifting for us
+                //  Massive hacks. Should work for all version of .net
+                //
+                // http://dotnetinside.com/framework/v2.0.50727/System.Web/StaticFileHandler
+                // http://typedescriptor.net/browse/types/7243-System.Web.StaticFileHandler
+                // http://stackoverflow.com/questions/7829478/how-to-execute-a-private-static-method-with-optional-parameters-via-reflection
+                // 
+                //
+                var genEtag = typeof(System.Web.StaticFileHandler).GetMethod("GenerateETag", BindingFlags.Static | BindingFlags.NonPublic);
+                string etag = genEtag.Invoke(obj: null, parameters: new object[] { HttpContext.Current, dateTime, now });
+
+                var rangeRequest = typeof(System.Web.StaticFileHandler).GetMethod("ProcessRangeRequest", BindingFlags.Static | BindingFlags.NonPublic);
+                if (StringUtil.StringStartsWithIgnoreCase(range, "bytes") && rangeRequest.Invoke(obj: null, parameters: new object[] { HttpContext.Current, filePath, file.Length, range, etag, dateTime })) {
+                    return;
+                }
                 response.AddHeader("Content-Length", file.Length.ToString());
+                response.AppendHeader("Accept-Ranges", "bytes");
+                response.Cache.SetIgnoreRangeRequests();
 
 
                 //
